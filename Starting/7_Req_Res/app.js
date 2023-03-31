fe2d2c21166af7c90c83fcd772e30b7b880d2b80
Suriyaprakash-Github <yoursgump@gmail.com>
2023-04-01 00:18:10 +0530

    7_Req_Res

---
+++ Starting/7_Req_Res/app.js
@@ -0,0 +1,26 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/home") {
+    res.write("<html>");
+    res.write("<head><title>Response</title></head>");
+    res.write("<body><h1>Welcome Home</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else if (url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>Response</title></head>");
+    res.write("<body><h1> Welcome to About Us page</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else if (url === "/node") {
+    res.write("<html>");
+    res.write("<head><title>Response</title></head>");
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+});
+
+server.listen(4000);
