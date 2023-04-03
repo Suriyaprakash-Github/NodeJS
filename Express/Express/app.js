034698f09198d26aa4a5e26369ab52d400f3a312
Suriyaprakash-Github <yoursgump@gmail.com>
2023-04-03 13:06:51 +0530

    2.Middlewares

--- Express/Express/app.js
+++ Express/Express/app.js
@@ -1,16 +1,25 @@
 const http = require("http");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><input type='number' name='quantity'><button>Add Product</button></form>"
+  );
 });
-app.use((req, res, next) => {
-  console.log("in 2nd middleware");
-  res.send("{ key1: value }");
+
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>From Express</h1>");
 });
 
 app.listen(4000);
