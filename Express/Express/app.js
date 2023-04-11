9f097264a155f74b7f17d35c3fc50ddd720067c5
Suriyaprakash-Github <yoursgump@gmail.com>
2023-04-11 12:14:05 +0530

    task 5

--- Express/Express/app.js
+++ Express/Express/app.js
@@ -1,51 +1,75 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const fs = require("fs");
+
+// const app = express();
+
+// app.use(bodyParser.urlencoded());
+
+// app.get("/", (req, res) => {
+//   fs.readFile("username.txt", (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       data = "No chat exist";
+//     }
+//     res.send(
+//       `${data}<form action="/" method="POST" onSubmit="document.getElementById('username').value = localStorage.getItem('username')">
+//                 <input type="text" name="message" id="message">
+//                 <input type="hidden" name="username" id="username" placeholder="Enter username">
+//                 <br />
+//                 <button type="submit">Send</button>
+//             </form>`
+//     );
+//   });
+// });
+
+// app.post("/", (req, res) => {
+//   fs.writeFile(
+//     "username.txt",
+//     `${req.body.username}: ${req.body.message}`,
+//     { flag: "a" },
+//     (err) => (err ? console.log(err) : null)
+//   );
+
+//   res.redirect("/");
+// });
+
+// app.get("/login", (req, res) => {
+//   res.send(
+//     `<form action="/login" method="POST" onSubmit="localStorage.setItem('username', document.getElementById('username').value)">
+//         <input type="text" name="username" placeholder="username" id="username">
+//         <br />
+//         <button type="submit">Login</button>
+//     </form>`
+//   );
+// });
+
+// app.post("/login", (req, res) => {
+//   res.redirect("/");
+// });
+
+// app.listen(3000);
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const fs = require("fs");
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const contactusRoutes = require("./routes/contactus");
+const successRoutes = require("./routes/success");
 
-app.get("/", (req, res) => {
-  fs.readFile("username.txt", (err, data) => {
-    if (err) {
-      console.log(err);
-      data = "No chat exist";
-    }
-    res.send(
-      `${data}<form action="/" method="POST" onSubmit="document.getElementById('username').value = localStorage.getItem('username')">
-                <input type="text" name="message" id="message">
-                <input type="hidden" name="username" id="username" placeholder="Enter username">
-                <br />
-                <button type="submit">Send</button>
-            </form>`
-    );
-  });
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.post("/", (req, res) => {
-  fs.writeFile(
-    "username.txt",
-    `${req.body.username}: ${req.body.message}`,
-    { flag: "a" },
-    (err) => (err ? console.log(err) : null)
-  );
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(contactusRoutes);
+app.use(successRoutes);
 
-  res.redirect("/");
-});
-
-app.get("/login", (req, res) => {
-  res.send(
-    `<form action="/login" method="POST" onSubmit="localStorage.setItem('username', document.getElementById('username').value)">
-        <input type="text" name="username" placeholder="username" id="username">
-        <br />
-        <button type="submit">Login</button>
-    </form>`
-  );
-});
-
-app.post("/login", (req, res) => {
-  res.redirect("/");
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
--- Express/Express/routes/contactus.js
+++ Express/Express/routes/contactus.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const path = require("path");
-
 const rootDir = require("../util/path");
 
 const router = express.Router();
 
-router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+router.get("/contactus", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "contactus.html"));
 });
-
-router.post("/add-product", (req, res, next) => {
+router.post("/contactus", (req, res, next) => {
   console.log(req.body);
-  res.redirect("/");
+  res.redirect("/success");
 });
 
 module.exports = router;
