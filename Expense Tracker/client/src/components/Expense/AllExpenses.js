2ab0d43d4c8e8934f2ddcc9b69043283cace94c4
Suriyaprakash-Github <yoursgump@gmail.com>
2023-05-22 13:51:36 +0530

    showed expenses on screen

--- Expense Tracker/client/src/components/Expense/AllExpenses.js
+++ Expense Tracker/client/src/components/Expense/AllExpenses.js
@@ -1,26 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const AllExpenses = () => {
+  const expenses = useSelector((state) => state.expense.expenses);
+  console.log("from allExpenses", expenses);
   return (
     <>
       <div>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
-        possimus soluta inventore a, dolorum enim totam ut corporis. Dolorem,
-        velit. Earum placeat quisquam dignissimos in amet maiores officiis
-        corporis sed? Impedit reprehenderit soluta nihil nisi libero sequi cum
-        consequatur debitis? Voluptatem cum ex dicta totam! Voluptatem iste quas
-        magni laboriosam facilis dignissimos quisquam consequatur eligendi,
-        nostrum officia veritatis quidem animi. Commodi tenetur, error
-        aspernatur vero necessitatibus illum eligendi dicta veritatis quibusdam
-        dolorem minus iste aliquid cumque nostrum natus totam sequi impedit
-        asperiores itaque fugit at ea? Minima deleniti voluptatem dignissimos.
-        Dignissimos accusantium aut facere sunt corporis alias, laudantium
-        pariatur deserunt quaerat dolor quos beatae exercitationem nulla
-        provident enim quo adipisci dicta ipsam et sint. Placeat, debitis
-        cumque! Quo, maiores facilis. Officia perferendis, pariatur laboriosam
-        provident eligendi natus perspiciatis facere minima cupiditate ipsa qui
-        doloribus ut assumenda autem voluptatibus itaque sunt eos porro.
-        Eligendi ad, explicabo et voluptate perspiciatis quidem eveniet?
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Amount</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {expenses.map((expense) => (
+              <tr>
+                <td>{expense.name}</td>
+                <td>{expense.amount}</td>
+                <td>{expense.category}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
