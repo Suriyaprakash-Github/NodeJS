ed82ec2ec50e651fd770ae5b64520b32b47bf47d
Suriyaprakash-Github <yoursgump@gmail.com>
2023-06-29 12:43:41 +0530

    db env

--- ExpenseNew/server/databases/db.js
+++ ExpenseNew/server/databases/db.js
@@ -1,8 +1,14 @@
 const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize("expensenew", "root", "1@mLegend", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  process.env.MYSQL_DBNAME,
+  process.env.MYSQL_USERNAME,
+  process.env.MYSQL_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+  }
+);
 
 module.exports = sequelize;
