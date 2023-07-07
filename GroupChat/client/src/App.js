e26ddb53332026427095b31c67513053387d1b8b
Suriyaprakash-Github <yoursgump@gmail.com>
2023-07-07 11:18:00 +0530

    groupchat signup frontend

--- GroupChat/client/src/App.js
+++ GroupChat/client/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Login from "./components/Login/Login";
 
 function App() {
-  return <div>Group Chat App</div>;
+  return (
+    <>
+      <div>
+        Group Chat App
+        <Login />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ GroupChat/client/src/components/Login/Login.jsx
@@ -0,0 +1,110 @@
+import React, { useRef, useState } from "react";
+import classes from "./../styles/Login.module.css";
+
+const Login = () => {
+  const usernameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const phoneRef = useRef();
+
+  const [loginSwitch, setLoginSwitch] = useState(false);
+  const loginSwitchHandler = () => {
+    setLoginSwitch((prev) => !prev);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(
+      usernameRef.current.value,
+      emailRef.current.value,
+      phoneRef.current.value,
+      passwordRef.current.value
+    );
+  };
+
+  return (
+    <>
+      <section className={classes.loginContainer}>
+        <div className={classes.loginBackground}>
+          <section className={classes.l_leftContainer}>
+            <div>
+              <p> WelCome to Group Chat Application Demo</p>
+            </div>
+          </section>
+          <section className={classes.l_rightContainer}>
+            <form
+              action=""
+              onSubmit={formSubmitHandler}
+              className={classes.loginForm}
+            >
+              <div className={classes.formControl}>
+                <label htmlFor="username">Username: </label>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  ref={usernameRef}
+                  required
+                />
+              </div>
+
+              <div className={classes.formControl}>
+                <label htmlFor="email">Email: </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  ref={emailRef}
+                  required
+                />
+              </div>
+
+              <div className={classes.formControl}>
+                <label htmlFor="password">Phone Number: </label>
+                <input
+                  type="phone"
+                  name="phone"
+                  id="phone"
+                  ref={phoneRef}
+                  required
+                />
+              </div>
+
+              <div className={classes.formControl}>
+                <label htmlFor="password">Password: </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  ref={passwordRef}
+                  required
+                />
+              </div>
+
+              <div className={classes.formControlButton}>
+                {loginSwitch ? (
+                  <button type="submit">Login</button>
+                ) : (
+                  <button type="submit">Signup</button>
+                )}
+              </div>
+            </form>
+            {loginSwitch ? (
+              <p>
+                Don't have an account?
+                <button onClick={loginSwitchHandler}>Signup</button>
+              </p>
+            ) : (
+              <p>
+                Already have an account?
+                <button onClick={loginSwitchHandler}>Login</button>
+              </p>
+            )}
+          </section>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
