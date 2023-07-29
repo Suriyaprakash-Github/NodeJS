ffc888d30a60eb05189f62c4f542d83ce6574536
Suriyaprakash-Github <yoursgump@gmail.com>
2023-07-29 17:58:21 +0530

    add to cart

--- NoSQL/app.js
+++ NoSQL/app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("64c4e5df23c0a2866dc7fd2f")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
--- NoSQL/models/user.js
+++ NoSQL/models/user.js
@@ -1,17 +1,39 @@
 const mongodb = require("mongodb");
-const getDb = require("../util/database").getDb;
-
+const { getDb } = require("../util/database");
 const ObjectId = mongodb.ObjectId;
-
 class User {
-  constructor(username, email) {
-    this.name = username;
+  constructor(name, email, cart, id) {
+    this.name = name;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
-
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    const cart = [this.cart.items];
+    const cartProductIndex = cart.findIndex((cpi) => {
+      return cpi.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(productId),
+        quantity: newQuantity,
+      });
+    }
+    const updatedCart = { items: updatedCartItems };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne({ _id: this._id }, { $set: { cart: updatedCart } });
   }
 
   static findById(userId) {
@@ -19,11 +41,9 @@ class User {
     return db
       .collection("users")
       .findOne({ _id: new ObjectId(userId) })
-      .then((user) => {
-        console.log(user);
-        return user;
-      })
+      .then((user) => user)
       .catch((err) => console.log(err));
   }
 }
+
 module.exports = User;
